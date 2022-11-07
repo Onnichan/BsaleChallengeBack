7f4fe57ae18beba2794685706fdffe97621abbef
Daniel Aguilar <dyangeltk17@gmail.com>
2022-11-07 16:03:44 -0500

    :tada: initial commit

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+  const path = require("path").join("", "", ".env");
+  require("dotenv").config({ path });
+}
+
+module.exports = {
+  DATABASE: process.env.DATABASE,
+  HOST: process.env.HOST,
+  USER: proccess.env.USER,
+  PASSWORD: process.env.PASSWORD,
+}
